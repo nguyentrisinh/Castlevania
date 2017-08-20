3f3c0a3d7503d113694b0dd4e038ad76fccd909c
CongTran <minhcongkun@gmail.com>
2017-08-20 12:03:59 +0700

    Add Ghost

---
+++ CastlevaniaGame/Ghost.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _GHOST_H_
+#define _GHOST_H_
+
+#include "Enemy.h"
+
+class Ghost : 
+	public Enemy
+{
+public:
+	Ghost();
+	Ghost(LPD3DXSPRITE _SpriteHandler, World *_manager);
+	~Ghost();
+	virtual void Init(int _X, int _Y);
+	virtual void Update(const float &_DeltaTime);
+	virtual void Render();
+	virtual void Destroy();
+	virtual void Collision();
+
+	virtual void Init(int _X, int _Y, bool isRight);
+	virtual void CheckActive();
+
+};
+#endif 
